d78ec24a0e56589e8751c96fda804edfd594c134
Vikram <belvikram@gmail.com>
2018-10-19 22:43:04 -0500

    Added SCSS capabilities

--- src/App.js
+++ src/App.js
@@ -1,11 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './styles/common/app.css';
-import './styles/desktops/app.css';
-import './styles/mobile/app.css';
-import './styles/tablet/app.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'reactstrap';
 import LaunchPage from './launchPage/LaunchPage';
 class App extends Component {
   render() {
