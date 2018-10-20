1f16db2f1cb904507e539eb370624531f2223d6c
Vikram <belvikram@gmail.com>
2018-10-19 21:24:12 -0500

    add npm package build path to docs

---
+++ docs/precache-manifest.5d0dc84df79233f135d5c5dd69a4f244.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7e4dd8cb4ca51a2c7d4c",
+    "url": "/static/js/main.7e4dd8cb.chunk.js"
+  },
+  {
+    "revision": "1d2411be7a3441327e0d",
+    "url": "/static/js/1.1d2411be.chunk.js"
+  },
+  {
+    "revision": "7e4dd8cb4ca51a2c7d4c",
+    "url": "/static/css/main.b1d1fbdf.chunk.css"
+  },
+  {
+    "revision": "1d2411be7a3441327e0d",
+    "url": "/static/css/1.4b1d2a3e.chunk.css"
+  },
+  {
+    "revision": "5157e5df1181f0378bef900d475fb9f4",
+    "url": "/index.html"
+  }
+];
