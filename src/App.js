e4e5262cd472f9009075f931747f6985a83a3f15
Vikram <belvikram@gmail.com>
2018-10-19 21:02:55 -0500

    intial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './styles/common/app.css';
+import './styles/desktops/app.css';
+import './styles/mobile/app.css';
+import './styles/tablet/app.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'reactstrap';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className='main-cont'>
+            <div className='row cen name-cont'>
+              <span className="name">V</span> 
+              <span className="name">i</span> 
+              <span className="name">k</span>
+              <span className="name">r</span> 
+              <span className="name">a</span> 
+              <span className="name">m</span>
+                &nbsp;&nbsp;
+              <span className="name lName">B</span> 
+              <span className="name lName">e</span> 
+              <span className="name lName">l</span>
+              {/* <span className="name lName">d</span> 
+              <span className="name lName">e</span>   */}
+            </div>
+            <div className='cen links-cont'>
+              <a href="#" className="link">Portfolio</a> 
+              &nbsp;&nbsp; <span className='link-seperator'>| </span> &nbsp;&nbsp;
+              <a href="#" className="link">Blog</a> 
+              &nbsp;&nbsp; <span className='link-seperator'>| </span> &nbsp;&nbsp;
+              <a href="#" className="link">Apps</a> 
+              &nbsp;&nbsp; <span className='link-seperator'>| </span> &nbsp;&nbsp;
+              <a href="#" className="link">LinkedIn</a>
+              &nbsp;&nbsp; <span className='link-seperator'>| </span> &nbsp;&nbsp;
+              <a href="#" className="link">Code</a>
+            </div>
+        </div> 
+      </div>
+    );
+  }
+}
+
+export default App;
