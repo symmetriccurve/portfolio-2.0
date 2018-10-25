cd126717fe6b2fbe8d600e73710faa04617eca4c
Vikram <belvikram@gmail.com>
2018-10-25 00:00:16 -0500

    Added mobile and tablet media queries

--- src/blog/Blog.js
+++ src/blog/Blog.js
@@ -16,10 +16,14 @@ export default class Blog extends Component {
 					</div>
 					<div className="b-card__body">
 						<div className="b-card__title">
-                            This is a Sample Title
+                            <span className="b-card__title-text">
+                                Integrate React Into any web app using NPM
+                            </span> 
 						</div>
-						<div className="b-card__body2">
-                            This is a Sample body
+						<div className="b-card__description">
+                            <span className="b-card__description-text">
+                                Tutorial walks you through step by step to integrate react in any webapp Tutorial walks you through step by step to integrate react in any webapp
+                            </span>
 						</div>
 						<div className="b-card-buttons">
 							<div className="b-card-button">
@@ -29,8 +33,57 @@ export default class Blog extends Component {
                                 Read
 							</div>
 						</div>
-					</div>    
-
+                    </div>
+				</div>
+                <div className="b-cards__b-card">
+					<div className="b-card__date">
+						<span className="b-card__date-text"> 29th June 2018 </span>
+					</div>
+					<div className="b-card__body">
+						<div className="b-card__title">
+                            <span className="b-card__title-text">
+                                Integrate React Into any web app using NPM
+                            </span> 
+						</div>
+						<div className="b-card__description">
+                            <span className="b-card__description-text">
+                                Tutorial walks you through step by step to integrate react in any webapp Tutorial walks you through step by step to integrate react in any webapp
+                            </span>
+						</div>
+						<div className="b-card-buttons">
+							<div className="b-card-button">
+                                Share
+							</div>
+							<div className="b-card-button">
+                                Read
+							</div>
+						</div>
+                    </div>
+				</div>
+                <div className="b-cards__b-card">
+					<div className="b-card__date">
+						<span className="b-card__date-text"> 29th June 2018 </span>
+					</div>
+					<div className="b-card__body">
+						<div className="b-card__title">
+                            <span className="b-card__title-text">
+                                Integrate React Into any web app using NPM
+                            </span> 
+						</div>
+						<div className="b-card__description">
+                            <span className="b-card__description-text">
+                                Tutorial walks you through step by step to integrate react in any webapp Tutorial walks you through step by step to integrate react in any webapp
+                            </span>
+						</div>
+						<div className="b-card-buttons">
+							<div className="b-card-button">
+                                Share
+							</div>
+							<div className="b-card-button">
+                                Read
+							</div>
+						</div>
+                    </div>
 				</div>
 			</div>
 		</div>
