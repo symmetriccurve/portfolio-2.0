bed60514af719cfcf2d85f0b2f5bd03e5abc34a1
Vikram <belvikram@gmail.com>
2018-10-24 21:20:20 -0500

    added header and font variables

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import LaunchPage from './launchPage/LaunchPage';
+import Header from './header/Header';
+
 const App = () => {
     return (
 	<div className="App">
+		<Header />
 		<LaunchPage />
 	</div>
     );
--- src/header/Header.js
+++ src/header/Header.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react'
 
-const Header = () => {
+export default () => {
   return(
-	  <div>
-      This is Header
-	</div>
+	  <div class="header">
+		<div class="header__logo">
+			<div class="logo__name">
+            Vikram
+			</div>
+        &nbsp;
+			<div class="logo__name--last">
+            Bel
+			</div>
+		</div>
+		<div class="header__links">
+			<span class="links__link"> PortFolio</span>
+			<span class="links__link--seperator">|</span>
+			<span class="links__link"> Blog</span>
+			<span class="links__link--seperator">|</span>
+			<span class="links__link"> Code</span>
+		</div>
+	  </div>
   )
 }
