e2890c2bfcd12665111a82c54a16adc5e8e72e6e
Vikram <belvikram@gmail.com>
2018-10-27 15:17:40 -0500

    header alignment fixed, contact capcha func added, eslint single quote rule added

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -1,57 +1,135 @@
-{/*eslint-disable */}
-import React, { Component } from 'react'
-import Layout from '../layout/Layout'
+//{/*eslint-disable */}
+import React, { Component } from 'react';
 import ReCAPTCHA from 'react-google-recaptcha';
+import Layout from '../layout/Layout';
+const emailURL = 'https://script.google.com/macros/s/AKfycbyCnHfXJPBCQm17nGdtLa0immNulKNNJHzwWvyNhRg2bdIPmaU/exec'
 
-export default class Blog extends Component {
+export default class Contact extends Component {
+	state = {
+		greeterName: '',
+		contactDetail: '',
+		isCaptchaValidated: false,
+		messageStatus: 'not sent'
+	}
+
 	onChange(value) {
+		this.setState({
+			isCaptchaValidated: true
+		})
 		console.log('Captcha value:', value);
 	}
-  render() {
-    return (
-	<Layout>
-		<div class="contact">
-			<div class="contact__header--getInTouch">
+
+	validateFields(){
+		const { greeterName, contactDetail,isCaptchaValidated } = this.state
+		//FIXME: validate name and  email or phone
+		return greeterName && contactDetail && isCaptchaValidated
+	}
+
+	handleSend(){
+		const { greeterName, contactDetail,isCaptchaValidated } = this.state
+		if(greeterName && contactDetail && isCaptchaValidated){
+
+			const formData = new FormData();
+			formData.append('greeterName', greeterName);
+			formData.append('contactDetail', contactDetail);
+
+			fetch(emailURL,{
+				body: formData,
+				method: 'post'
+			}).then(res=>{
+				this.setState({
+					messageStatus: 'successfully sent'
+				})					
+			}).catch(err=>{
+				this.setState({
+					messageStatus: 'sent failed'
+				})
+			})
+		}
+	}
+
+	renderGreeterBlock(){
+		const { greeterName, contactDetail, messageStatus } = this.state
+		if(messageStatus == 'successfully sent'){
+			return(
+				<div className="greeter__success">
+					<div className="success__text">
+						Message Sent Successfully 
+					</div>
+				</div>
+			)
+		}
+
+		return(
+			<div>
+				<div className="contact__OR">
+						OR
+				</div>
+				<div className="contact__greeter">
+					<div className="greeter__sentence--my-name">
+						{' Hey Vikram, '}
+					</div>
+					<div className="greeter__sentence">
+							this is 
+					</div>
+					<input value={ greeterName } name="greeterName" onChange={ (event)=>this.handleChange(event) } className="greeter__input--name" placeholder="Name"/>
+					<br />
+					<div className="greeter__sentence">
+							let's have a quick chat at  
+					</div>
+					<input value={ contactDetail } name="contactDetail" onChange={ (event)=>this.handleChange(event) } className="greeter__input--email" placeholder="Email/Phone"/>
+				</div>
+				<div className="greeter__captcha">
+					<ReCAPTCHA
+					sitekey="6Ldu7XYUAAAAADj7U1LjSG8cJ5oHIe6dIWIQEhKK"
+					onChange={ (value)=>this.onChange(value) }
+				/>
+				</div>
+				{
+					this.validateFields() ?
+						<div className="greeter__send" onClick={ ()=>this.handleSend() }>
+							<span className="greeter__send__text"> SEND </span>
+						</div> :
+						<div className="greeter__send">
+							<span className="greeter__send__text--inactive"> SEND </span>
+						</div>
+
+				}
+			</div>			
+		)
+	}
+
+	handleChange(event){
+		const { name,value } = event.target
+		this.setState({
+			[ name ]: value
+		})
+	}
+
+	render(){
+		return(
+			<Layout>
+				<div className="contact">
+					<div className="contact__header--getInTouch">
 					GET IN TOUCH
-			</div>
-			<div class="contact__header">
+					</div>
+					<div className="contact__header">
 					Shoot an email to
-			</div>
-			<div class="contact__info">
+					</div>
+					<div className="contact__info">
 					belvikram@gmail.com
-			</div>
-			<div class="contact__header">
+					</div>
+					<div className="contact__header">
 					Or ring me on
-			</div>
-			<div class="contact__info">
+					</div>
+					<div className="contact__info">
 					+1-636-224-5062
-			</div>
-			<div class="contact__OR">
-					OR
-			</div>
-			<div class="contact__greeter">
-				<div class="greeter__sentence--my-name">
-					{' Hey Vikram, '}
+					</div>
+					<div className="contact-greeter">
+						{this.renderGreeterBlock()}
+					</div>
 				</div>
-				<div class="greeter__sentence">
-						this is 
-				</div>
-				<input class="greeter__input--name" placeholder="Name"/>
-				<br />
-				<div class="greeter__sentence">
-						let's have a quick chat at  
-				</div>
-				<input class="greeter__input--email" placeholder="Email/Phone"/>
-			</div>
-			<div className="greeter__send">
-					<span className='greeter__send__text'> SEND </span>
-				</div>
-			{/* <ReCAPTCHA
-								sitekey="6Ldu7XYUAAAAADj7U1LjSG8cJ5oHIe6dIWIQEhKK"
-								onChange={(value)=>this.onChange(value)}
-							/> */}
-		</div>
-	</Layout>
-    )
-  }
+			</Layout>
+		)
+	}
 }
--- src/header/Header.js
+++ src/header/Header.js
@@ -6,44 +6,44 @@ import {
 
 export default () => {
   return(
-	<div class="header">
+	<div className="header">
 		<div className='header__left'>
 			<NavLink to='/'>
-				<div class="header__logo">
-					<div class="logo__name">
+				<div className="header__logo">
+					<div className="logo__name">
 									Vikram
 					</div>
 							&nbsp;
-					<div class="logo__name--last">
+					<div className="logo__name--last">
 									Bel
 					</div>
 				</div>
 			</NavLink>
 		</div>
 		<div className='header__right'>		
-			<div class="header__links">
+			<div className="header__links">
 				<NavLink to='/blog' activeClassName="links__link--active links__link">
 						Blog
 				</NavLink>
-				<span class="links__link--seperator">|</span>
+				<span className="links__link--seperator">|</span>
 				<NavLink  exact to='/projects' activeClassName="links__link--active links__link">
 						Projects
 				</NavLink>
-				<span class="links__link--seperator">|</span>
+				<span className="links__link--seperator">|</span>
 				<a href="https://github.com/symmetriccurve/">
-					<span class="links__link">Code</span>
+					<span className="links__link">Code</span>
 				</a>
-				<span class="links__link--seperator">|</span>
+				<span className="links__link--seperator">|</span>
 				<NavLink  exact to='/contact' activeClassName="links__link--active links__link">
 						Contact
 				</NavLink>
-				<span class="links__link--seperator">|</span>
+				<span className="links__link--seperator">|</span>
 				<NavLink  exact to='/about-me' activeClassName="links__link--active links__link">
 						Picturize Me
 				</NavLink>
-				{/* <span class="links__link--seperator">|</span>
+				{/* <span className="links__link--seperator">|</span>
 					<a href="https://www.linkedin.com/in/vikrambelde/">
-						<span class="links__link">LinkedIn</span>
+						<span className="links__link">LinkedIn</span>
 					</a>	 */}
 			</div>
 		</div>		
