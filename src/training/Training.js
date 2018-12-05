82cb12408104c5db8161dff4c889397757fb91df
Vikram <belvikram@gmail.com>
2018-12-05 12:44:55 -0600

    page and route added

---
+++ src/training/Training.js
@@ -0,0 +1,16 @@
+{/*eslint-disable */}
+import React, { Component } from 'react';
+import ReCAPTCHA from 'react-google-recaptcha';
+import Layout from '../layout/Layout';
+const emailURL = 'https://script.google.com/macros/s/AKfycbyCnHfXJPBCQm17nGdtLa0immNulKNNJHzwWvyNhRg2bdIPmaU/exec'
+
+export default class Contact extends Component {
+
+	render(){
+		return(
+			<Layout>
+				
+			</Layout>
+		)
+	}
+}
