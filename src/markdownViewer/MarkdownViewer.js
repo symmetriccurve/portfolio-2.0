146ea66da24e2c0995d40d86b877d035a6b2838d
Vikram <belvikram@gmail.com>
2019-03-10 19:58:05 -0500

    added loader on API fetch

--- src/markdownViewer/MarkdownViewer.js
+++ src/markdownViewer/MarkdownViewer.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import ReactMarkdown from 'react-markdown';
-import CodeBlock from './renderers/codeblocks'
+import CodeBlock from './renderers/codeblocks';
+import Loader from 'react-loaders';
+import 'loaders.css/src/animations/ball-rotate.scss';
 export default class MarkDownViewer extends Component {
 
 	state = {
@@ -37,6 +39,11 @@ export default class MarkDownViewer extends Component {
 		markDown.forEach(each=>{
 			if(each.toLowerCase().indexOf(value.toLowerCase()) > -1 ){
 				//each = each.replace(value,`<mark>${ value }</mark>`)
+				/* 
+					TO-DO: FIX SEARCH
+					This highlight is not highlighting capital case match and replacing
+					text with in the code(A BIG NO NO)
+				*/
 				filtered.push(each)
 			}
 		})
@@ -49,7 +56,7 @@ export default class MarkDownViewer extends Component {
 
 	render() {
 		const { showSearchBar } = this.props
-		const { searchStr, filtered } = this.state
+		const { searchStr, filtered, markDown } = this.state
 		return (
 			<div className='markdown'>
 				{ 
@@ -57,6 +64,12 @@ export default class MarkDownViewer extends Component {
 					<div className="markdown__search-bar">
 						<input className="markdown__search-bar__input" placeholder='Search' value={ searchStr } onChange={ e=>this.handleSearch(e) } />
 					</div>
+				}
+				{
+					!markDown.length && 
+					<div>
+						<Loader className='markdown__loader' type="ball-rotate" />
+					</div>	
 				}
 				{
 					filtered.map(each => {
