4c4ec90762be5b53334bee9ebb9e16a21e2101bd
Vikram Belde <belvikram@gmail.com>
2019-11-07 10:39:20 -0600

    remove footer links

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,46 +1,51 @@
-import React,{ Component } from 'react'
+import React, { Component } from 'react'
 
-import {
+import
+{
 	NavLink
 } from 'react-router-dom'
 
-export default class componentName extends Component {
+export default class componentName extends Component
+{
 
 	state = {
 		lastUpdatedOn: 'December 14 2018'
 	}
 
-	componentDidMount() {
+	componentDidMount()
+	{
 		fetch('https://api.github.com/repos/symmetriccurve/portfolio-2.0/commits/master')
 			.then(res => res.json())
-			.then(res => {
+			.then(res =>
+			{
 				/*  
 				 	Date is in UTC format : 2018-12-11T18:57:07Z 
 					toString does to "Tue Dec 11 2018 13:57:07 GMT-0500 (Central Daylight Time)"
 					slice it to make "Dec 11 2018"
 				*/
-				  
+
 				let lastUpdatedOn = new Date(res.commit.committer.date)
-				lastUpdatedOn = lastUpdatedOn.toString().slice(4,-42)
+				lastUpdatedOn = lastUpdatedOn.toString().slice(4, -42)
 				this.setState({
 					lastUpdatedOn
 				})
 			})
 	}
 
-	render() {
+	render()
+	{
 		const { lastUpdatedOn } = this.state
 		return (
 			<div className="footer">
 				<div className="footer__left">
 					Copyright © 2019 Vikram Belde. All rights reserved
 					<span className="footer__left--lastUpdatedOnText">
-						 (Last Updated on {lastUpdatedOn})
-					</span>	
+						(Last Updated on {lastUpdatedOn})
+					</span>
 				</div>
 				<div className='footer__right'>
 					<div className="footer__links">
-						<NavLink exact to='/as-a-engineer' activeClassName="links__link--active links__link">
+						{/* <NavLink exact to='/as-a-engineer' activeClassName="links__link--active links__link">
 							As a Engineer
 						</NavLink>
 						<span className="links__link--seperator">|</span>
@@ -50,7 +55,7 @@ export default class componentName extends Component {
 						<span className="links__link--seperator">|</span>
 						<NavLink exact to='/today-i-learned' activeClassName="links__link--active links__link">
 							Today I Learned
-						</NavLink>
+						</NavLink> */}
 						{/* <span className="links__link--seperator">|</span>
 					<a href="https://www.linkedin.com/in/vikrambelde/">
 						<span className="links__link">LinkedIn</span>
