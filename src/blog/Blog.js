816d5c70a62f56cd1e37c99a80226674726e6ad4
vbelde.ctr@aspiration.com <vbelde.ctr@aspiration.com>
2021-03-28 19:15:11 -0500

    cleanup

--- src/blog/Blog.js
+++ src/blog/Blog.js
@@ -1,51 +1,40 @@
 import React, { Component } from "react";
-import ShowContentsHOC from "../hoc/ShowContent";
-import Loader from "react-loaders";
-import ReactMarkdown from "react-markdown";
-import CodeBlock from "../markdownViewer/renderers/codeblocks";
 import Layout from "../layout/Layout";
 import urls from "../data-layer/urls";
-import { Skeleton } from "antd";
-import post from "./posts/1-Interview Questions.md";
 import { Card, Avatar, Row, Col, Divider } from "antd";
-import { ReadFilled, SettingFilled } from "@ant-design/icons";
-import posts from './posts/index.json'
 import { Link } from "react-router-dom";
 const { Meta } = Card;
 export default class Blog extends Component {
   state = {
-    posts//: [],
+    posts: [],
   };
 
   componentDidMount() {
-    // fetch("https://raw.githubusercontent.com/symmetriccurve/portfolio-2.0/master/src/blog/posts/index.json")
-
-    // fetch(urls.blog)
-    //   .then((res) => res.json())
-    //   .then((posts) => {
-    //     // resText.forEach((each) => {
-    //     //   fetch(each.download_url)
-    //     //     .then((post) => post.text())
-    //     //     .then((postAsText) => {
-    //     this.setState({
-    //       posts,
-    //     });
-    //   })
-    //   //     .catch((err) => {
-    //   //       console.log("error", err);
-    //   //     });
-    //   // });
-    //   // })
-    //   .catch((err) => {
-    //     console.log("error", err);
-    //   });
+    fetch(urls.blog)
+      .then((res) => res.json())
+      .then((posts) => {
+        this.setState({
+          posts,
+        });
+      })
+      .catch((err) => {
+        console.log("error", err);
+      });
   }
 
   render() {
     const { posts } = this.state;
     return (
       <Layout>
-        <div className="markdown" style={{display:"flex", alignItems: "center",justifyContent: "center", margin:'20px'}}>
+        <div
+          className="markdown"
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            margin: "20px",
+          }}
+        >
           {!posts.length && (
             <Row>
               <Col>
@@ -62,20 +51,21 @@ export default class Blog extends Component {
               </Col>
             </Row>
           )}
-          <Row style={{display: 'flex', justifyContent: 'center'}}>
+          <Row style={{ display: "flex", justifyContent: "center" }}>
             {posts.map((each) => {
               return (
-                <Link to={`/post&postId=${each.id}`} params={{ postId: each.id }} key={each.id}>
-                  <Col style={{margin:'15px'}}>
+                <Link
+                  to={`/post&postId=${each.id}`}
+                  params={{ postId: each.id }}
+                  key={each.id}
+                >
+                  <Col style={{ margin: "15px" }}>
                     <Card
                       hoverable
                       style={{ width: 300 }}
                       cover={<img alt="post-image" src={each.imageSrc} />}
                     >
-                      <Meta
-                        title={each.title}
-                        description={each.subTitle}
-                      />
+                      <Meta title={each.title} description={each.subTitle} />
                     </Card>
                   </Col>
                 </Link>
@@ -87,52 +77,3 @@ export default class Blog extends Component {
     );
   }
 }
-
-// const all_posts_urls = await fetch(urls.blog)
-// .then((res) => res.json())
-// .then((res) => res)
-// .catch((err) => {
-//   console.log("error", err);
-// });
-// debugger;
-// const allFetches = [];
-
-// all_posts_urls.forEach((postObj) => {
-// allFetches.push(fetch(postObj.download_url));
-// });
-
-// const allRequests = all_posts_urls.map((url) =>
-// fetch(url)
-//   .then((response) => response.json())
-//   .then((res) => {
-// 	debugger;
-// 	return res;
-//   })
-// );
-
-//const data = Promise.all(allRequests);
-//   .then((result) => Promise.all([result[0].json()]))
-//   .then((res) => {
-//     debugger;
-//   })
-// .catch((error) => console.log(`Error in promises ${error}`));
-// fetch(urls.blog)
-//   .then((res) => res.json())
-//   .then((resText) => {
-//     resText.forEach((each) => {
-//       fetch(each.download_url)
-//         .then((post) => post.text())
-//         .then((postAsText) => {
-//           this.setState({
-//             postsAsText: [...this.state.postsAsText, postAsText],
-//           });
-//         })
-//         .catch((err) => {
-//           console.log("error", err);
-//         });
-//     });
-//   })
-//   .catch((err) => {
-//     console.log("error", err);
-//   });
-//}
